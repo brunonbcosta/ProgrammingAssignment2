554eb36e9c587f49795b525b3e93326025c740d6
brunonbcosta <40046010+brunonbcosta@users.noreply.github.com>
2018-06-24 20:28:50 -0300

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,29 @@
+# As funções são usadas para armazenar em cache o inverso de uma matriz.
+
+# Obter o valor do inverso da matriz
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+
+# A função retorna o inverso da matriz, verificando primeiro seo inverso já foi calculado.
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
